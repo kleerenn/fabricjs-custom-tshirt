e09603ad193548c43fb2967d2966bcbdade1cc39
kleerenn <hjlee8820@gmail.com>
2020-03-10 09:21:36 +0900

    chore: setup a project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <custom-tshirt></custom-tshirt>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CustomTshirt from './components/CustomTshirt.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CustomTshirt
   }
 }
 </script>
---
+++ src/components/CustomTshirt.vue
@@ -0,0 +1,61 @@
+<template>
+    <div id="tshirt-div">
+        <img id="tshirt-backgroundpicture" src="../assets/img/bg-tshirt.png"/>
+        <div id="drawingArea" class="drawing-area">
+            <div class="canvas-container">
+                <canvas id="canvas" width="200" height="400"></canvas>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { fabric } from 'fabric'
+    export default {
+        name: "CustomTshirt",
+        data() {
+            return {
+                canvas: null
+
+            }
+        },
+        beforeMount() {
+             this.canvas = new fabric.Canvas('canvas')
+        }
+    }
+</script>
+
+<style scoped>
+.drawing-area{
+    position: absolute;
+    top: 60px;
+    left: 122px;
+    z-index: 10;
+    width: 200px;
+    height: 400px;
+}
+
+.canvas-container{
+    width: 200px;
+    height: 400px;
+    position: relative;
+    user-select: none;
+}
+
+#tshirt-div{
+    width: 452px;
+    height: 548px;
+    position: relative;
+    background-color: #fff;
+}
+
+#canvas{
+    position: absolute;
+    width: 200px;
+    height: 400px;
+    left: 0px;
+    top: 0px;
+    user-select: none;
+    cursor: default;
+}
+</style>
